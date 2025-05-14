3f32eb7f15da089156c7db178710c0ea48638b42
tranvanngan <ngantran.11072005nd@gmail.com>
2025-05-15 00:10:58 +0700

    oki nha

---
+++ BasicGroceryStore/DataAccessLayer/DAO_Information.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Security.Principal;
+using System.Xml;
+
+namespace BasicGroceryStore
+{
+    /// <summary>
+    /// Using for save store information and remember account (with xml file)
+    /// </summary>
+    public class DAO_Information
+    {
+        private XmlDocument doc = new XmlDocument();
+        private XmlElement root;
+
+        private string path = Path.GetDirectoryName(Directory.GetCurrentDirectory());
+        private string using_path;
+
+        public DAO_Information(string fileName)
+        {
+            path = path.Replace("bin", "Data/");
+            using_path = path + fileName;
+            doc.Load(using_path);
+            root = doc.DocumentElement;
+        }
+
+        /// <summary>
+        /// Get new information of store
+        /// </summary>
+        /// <param name="data"></param>
+        public void UpdateInformation(Dictionary<string, string> data = null)
+        {
+            XmlNode new_information = doc.CreateElement("information");
+
+            XmlNode new_address = doc.CreateElement("address");
+            new_address.InnerText = data["address"];
+            new_information.AppendChild(new_address);
+
+            XmlNode new_email = doc.CreateElement("email");
+            new_email.InnerText = data["email"];
+            new_information.AppendChild(new_email);
+
+            XmlNode new_phone = doc.CreateElement("phone");
+            new_phone.InnerText = data["phone"];
+            new_information.AppendChild(new_phone);
+
+            // LINK
+            XmlNode new_link = doc.CreateElement("link");
+
+            XmlNode new_link_map = doc.CreateElement("map");
+            new_link_map.InnerText = data["link_map"];
+            new_link.AppendChild(new_link_map);
+
+            XmlNode new_link_fb = doc.CreateElement("facebook");
+            new_link_fb.InnerText = data["link_fb"];
+            new_link.AppendChild(new_link_fb);
+
+            XmlNode new_link_ig = doc.CreateElement("instagram");
+            new_link_ig.InnerText = data["link_ig"];
+            new_link.AppendChild(new_link_ig);
+
+            XmlNode new_link_linkedin = doc.CreateElement("linkedin");
+            new_link_linkedin.InnerText = data["link_linkedin"];
+            new_link.AppendChild(new_link_linkedin);
+
+            XmlNode new_link_ut = doc.CreateElement("youtube");
+            new_link_ut.InnerText = data["link_ut"];
+            new_link.AppendChild(new_link_ut);
+
+            new_information.AppendChild(new_link);
+
+            root.ReplaceChild(new_information, root.FirstChild);
+            doc.Save(using_path);
+        }
+
+        /// <summary>
+        /// Take information and bring it to GUI
+        /// </summary>
+        /// <returns></returns>
+        public Dictionary<string, string> LoadStoreInformation()
+        {
+            Dictionary<string, string> information = new Dictionary<string, string>();
+
+            XmlNode xml_data = root.FirstChild;
+            information["address"] = xml_data["address"].InnerText;
+            information["email"] = xml_data["email"].InnerText;
+            information["phone"] = xml_data["phone"].InnerText;
+
+            information["link_map"] = xml_data["link"].ChildNodes[0].InnerText;
+            information["link_fb"] = xml_data["link"].ChildNodes[1].InnerText;
+            information["link_ig"] = xml_data["link"].ChildNodes[2].InnerText;
+            information["link_linkedin"] = xml_data["link"].ChildNodes[3].InnerText;
+            information["link_ut"] = xml_data["link"].ChildNodes[4].InnerText;
+
+            return information;
+        }
+
+        /// <summary>
+        /// Update data in Account.xml
+        /// </summary>
+        /// <param name="acc"></param>
+        /// <param name="isSave"></param>
+        public void UpdateSaveAccount(Account acc, bool isSave)
+        {
+            XmlNode new_data = doc.CreateElement("Account");
+
+            XmlNode staff_id = doc.CreateElement("staff_id");
+            staff_id.InnerText = "";
+
+            XmlNode username = doc.CreateElement("username");
+            username.InnerText = "";
+
+            XmlNode password = doc.CreateElement("password");
+            password.InnerText = "";
+
+            XmlNode is_save = doc.CreateElement("is_save");
+            is_save.InnerText = isSave.ToString();
+
+            if (isSave)
+            {
+                staff_id.InnerText = acc.Staff_id;
+                username.InnerText = acc.Username;
+                password.InnerText = acc.Password;
+            }
+
+            new_data.AppendChild(staff_id);
+            new_data.AppendChild(username);
+            new_data.AppendChild(password);
+            new_data.AppendChild(is_save);
+
+            root.ReplaceChild(new_data, root.FirstChild);
+            doc.Save(using_path);
+        }
+
+        /// <summary>
+        /// Take SaveAccount in Account.xml and bring it to Login form 
+        /// </summary>
+        /// <returns></returns>
+        public List<object> LoadSaveAccount()
+        {
+            List<object> data = new List<object>();
+
+            XmlNode xml_data = root.FirstChild;
+            data.Add(xml_data["staff_id"].InnerText);
+            data.Add(xml_data["username"].InnerText);
+            data.Add(xml_data["password"].InnerText);
+            data.Add(xml_data["is_save"].InnerText);
+
+            return data;
+        }
+    }
+}using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicGroceryStore.DataAccessLayer
+{
+    internal class DAO_Information
+    {
+    }
+}
---
+++ BasicGroceryStore/DataAccessLayer/DAO_Product.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+
+namespace BasicGroceryStore
+{
+    internal class DAO_Product : IControl<Product>, IProductServices
+    {
+        public bool Create(Product product)
+        {
+            return (DataProvider.Instance.ExecuteNonQuery("sp_InsertProduct",
+                    CommandType.StoredProcedure,
+                    new SqlParameter("@ID", product.ID),
+                    new SqlParameter("@Name", product.Name),
+                    new SqlParameter("@TypeProduct", product.TypeProduct),
+                    new SqlParameter("@Unit", product.Unit),
+                    new SqlParameter("@Quantity", product.Quantity),
+                    new SqlParameter("@Image", Convert.ImageToByteArray(product.Image)),
+                    new SqlParameter("@Note", product.Note),
+                    new SqlParameter("@Price", product.Price),
+                    new SqlParameter("@SupplierID", product.SupplierID)) > 0) ? true : false;
+        }
+
+        public bool Update(Product product)
+        {
+            return (DataProvider.Instance.ExecuteNonQuery("sp_UpdateProduct",
+                     CommandType.StoredProcedure,
+                     new SqlParameter("@ID", product.ID),
+                     new SqlParameter("@Name", product.Name),
+                     new SqlParameter("@TypeProduct", product.TypeProduct),
+                     new SqlParameter("@Unit", product.Unit),
+                     new SqlParameter("@Quantity", product.Quantity),
+                     new SqlParameter("@Image", Convert.ImageToByteArray(product.Image)),
+                     new SqlParameter("@Note", product.Note),
+                     new SqlParameter("@Price", product.Price),
+                     new SqlParameter("@SupplierID", product.SupplierID)) > 0) ? true : false;
+        }
+
+        public bool Delete(Product product)
+        {
+            return (DataProvider.Instance.ExecuteNonQuery("sp_DeleteProduct",
+                    CommandType.StoredProcedure, new SqlParameter("@id", product.ID)) > 0) ? true : false;
+        }
+
+        public DataTable GetAllProduct()
+        {
+            return DataProvider.Instance.ExecuteQuery("sp_GetAllProduct", CommandType.StoredProcedure, null);
+        }
+
+        public Product GetProduct(string product_id)
+        {
+            if (product_id == "")
+                return null;
+
+            DataTable dt = DataProvider.Instance.ExecuteQuery($"select * from Product where Id='{product_id}'", CommandType.Text, null);
+            DataRow row = dt.Rows[0];
+
+            Image image = null;
+            if (row[5] != DBNull.Value)
+            {
+                image = Convert.ByteArrayToImage((byte[])row[5]);
+            }
+
+            Product product = new Product(
+                iD: row[0].ToString(),
+                name: row[1].ToString(),
+                typeProduct: row[2].ToString(),
+                unit: row[3].ToString(),
+                quantity: int.Parse(row[4].ToString()),
+                image: image,
+                note: row[6].ToString(),
+                price: float.Parse(row[7].ToString()),
+                supplierID: row[8].ToString());
+            return product;
+        }
+
+        public DataTable GetProductOfSupplier(string supplier_id)
+        {
+            return DataProvider.Instance.ExecuteQuery("select * from Product where SupplierID = @sup_id", CommandType.Text, new SqlParameter("@sup_id", supplier_id));
+        }
+
+        public List<string> GetAllTypeOfProduct()
+        {
+            List<string> typeProduct = new List<string>();
+            DataTable tb = DataProvider.Instance.ExecuteQuery("select distinct TypeProduct from Product", CommandType.Text);
+            for (int i = 0; i < tb.Rows.Count; i++)
+            {
+                typeProduct.Add(tb.Rows[i][0].ToString());
+            }
+            return typeProduct;
+        }
+
+        public List<string> GetAllUnit()
+        {
+            List<string> unit = new List<string>();
+            DataTable tb = DataProvider.Instance.ExecuteQuery("select distinct Unit from Product", CommandType.Text);
+            for (int i = 0; i < tb.Rows.Count; i++)
+            {
+                unit.Add(tb.Rows[i][0].ToString());
+            }
+            return unit;
+        }
+
+        public int GetNumberOfProduct_depot()
+        {
+            return (int)DataProvider.Instance.ExecuteScalar("select dbo.func_NumberOfProduct_depot()", CommandType.Text);
+        }
+
+        public DataTable FindProductByTypeProduct(string typeProduct)
+        {
+            return DataProvider.Instance.ExecuteQuery($"select * from dbo.func_FindProductByTypeProduct(N'{typeProduct}')", CommandType.Text);
+        }
+
+        public DataTable FindProductByName(string name)
+        {
+            return DataProvider.Instance.ExecuteQuery($"select * from dbo.func_FindProductByName(N'{name}')", CommandType.Text);
+        }
+
+        public DataTable FindProductByPriceRange(float from, float to)
+        {
+            return DataProvider.Instance.ExecuteQuery($"select * from dbo.func_FindProductByPriceRange({from}, {to})", CommandType.Text);
+        }
+
+        public DataTable FindProductBySupplier(string supplierName)
+        {
+            return DataProvider.Instance.ExecuteQuery($"select * from dbo.func_FindProductBySupplier(N'{supplierName}')", CommandType.Text);
+        }
+    }
+}
